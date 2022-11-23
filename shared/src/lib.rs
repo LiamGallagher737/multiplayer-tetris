5bd0f761cefd39b415ed900a30b6ee0fa57261b2
Liam Gallagher <ljpgallagher@xtra.co.nz>
2022-11-24 10:57:50 +1300

    Shared Messages

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,3 +1,36 @@
-pub enum Msg {
-    
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ClientMessage {
+    StartGame, // Start the Game
+    OtherTetrisMove(TetrisMove), // Other player peice moved
+    BallUpdate { // Update pong ball
+        position: [f32; 2],
+        velocity: [f32; 2],
+    },
+    StateUpdate(GameState),
+    GameEnd(u8), // End the game | u8: Winning Player
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ServerMessage {
+    TetrisMove(TetrisMove),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum TetrisMove {
+    Left, // Move Left
+    Right, // Move Right
+    Drop, // Normal Falling
+    SoftDrop, // Player Fall
+    HardDrop, // Tp to Bottom
+    Rotate(bool), // Rotate | bool: Direction
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub enum GameState {
+    #[default]
+    Normal,
+    Hyper,
+    Swap,
 }
